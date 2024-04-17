a8931405f9a8c2f55861db125ef851d23331db2a
taufiq hidayat <taufiqhidayat140504@gmail.com>
2024-04-17 15:43:50 +0700

    taufiq push

---
+++ lib/main.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: const Color.fromARGB(255, 255, 255, 255),
+        appBar: AppBar(
+          title: const Text("Booking"),
+        ),
+        body: const Center(
+          child: Text("Hello World!"),
+        ),
+        bottomNavigationBar: MyBottomNavigationBar(),
+      ),
+    );
+  }
+}
+
+class MyBottomNavigationBar extends StatefulWidget {
+  @override
+  _MyBottomNavigationBarState createState() => _MyBottomNavigationBarState();
+}
+
+class _MyBottomNavigationBarState extends State<MyBottomNavigationBar> {
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      items: const <BottomNavigationBarItem>[
+        BottomNavigationBarItem(
+          icon: Icon(Icons.home_outlined, size : 24),
+          label: 'Home',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.book_outlined, size : 24),
+          label: 'Booking',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.browse_gallery_outlined, size : 24),
+          label: 'Activity',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.account_circle_outlined, size : 24),
+          label: 'Account',
+        ),
+      ],
+      currentIndex: _selectedIndex,
+      selectedItemColor: const Color(0xff0F1035), // Change this to your preferred color
+      unselectedItemColor: Colors.grey, // Set the unselected item color here
+      showUnselectedLabels: true,
+      onTap: _onItemTapped,
+    );
+  }
+}
