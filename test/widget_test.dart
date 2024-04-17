5067e5326e3e92f9989891dc99c6d3e183ded675
taufiq hidayat <taufiqhidayat140504@gmail.com>
2024-04-17 15:45:45 +0700

    Rafie Push

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:tubes_kel_1/activity.dart';
+import 'package:medtech/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
